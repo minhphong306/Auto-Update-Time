65b782b7ccbc9101913d8f0b6c71de364c2eba8e
minhphong306 <dominhphong306@gmail.com>
2017-06-29 01:20:36 +0700

    First commit - version 1.0

---
+++ AutoUpdateTime/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AutoUpdateTime {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServerAdd = new System.Windows.Forms.TextBox();
+            this.txtUpdatedTime = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(22, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(720, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chương trình tự động cập nhật thời gian – blog minhphong306";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Bây giờ là";
+            // 
+            // txtServerAdd
+            // 
+            this.txtServerAdd.Location = new System.Drawing.Point(197, 86);
+            this.txtServerAdd.Name = "txtServerAdd";
+            this.txtServerAdd.Size = new System.Drawing.Size(423, 29);
+            this.txtServerAdd.TabIndex = 2;
+            // 
+            // txtUpdatedTime
+            // 
+            this.txtUpdatedTime.Location = new System.Drawing.Point(197, 126);
+            this.txtUpdatedTime.Name = "txtUpdatedTime";
+            this.txtUpdatedTime.Size = new System.Drawing.Size(423, 29);
+            this.txtUpdatedTime.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(278, 189);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(128, 45);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(763, 262);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtUpdatedTime);
+            this.Controls.Add(this.txtServerAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Auto update time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServerAdd;
+        private System.Windows.Forms.TextBox txtUpdatedTime;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ AutoUpdateTime/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoUpdateTime {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SYSTEMTIME st);
+
+        private DateTime getOnlineDate(String server) {
+            var myHttpWebRequest = (HttpWebRequest)WebRequest.Create(server);
+            var response = myHttpWebRequest.GetResponse();
+            string todaysDates = response.Headers["date"];
+            return DateTime.ParseExact(todaysDates,
+                                       "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                                       CultureInfo.InvariantCulture.DateTimeFormat,
+                                       DateTimeStyles.AssumeUniversal);
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string server = txtServerAdd.Text;
+
+            if (String.IsNullOrEmpty(server))
+            {
+                MessageBox.Show("Vui lòng nhập vào địa chỉ server");
+
+            }
+            try
+            {
+                var date_online = getOnlineDate(server);
+                var date_converted = date_online.ToUniversalTime();
+
+                SYSTEMTIME st = new SYSTEMTIME {
+                    wYear = (short)date_converted.Year,
+                    wMonth = (short)date_converted.Month,
+                    wDay = (short)date_converted.Day,
+                    wHour = (short)(date_converted.Hour),
+                    wMinute = (short)date_converted.Minute,
+                    wSecond = (short)date_converted.Second,
+                    wMilliseconds = (short)date_converted.Millisecond
+                };
+
+                SetSystemTime(ref st);
+
+                txtUpdatedTime.Text = date_online.ToLongTimeString() + " - " + date_online.ToLongDateString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Server đã bị ngỏm hoặc có lỗi xảy ra.\nVui lòng nhập server khác.");
+                return;
+            }
+            
+        }
+    }
+}
